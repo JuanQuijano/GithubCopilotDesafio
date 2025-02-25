9176eb7cacb5f14d432ef1e21c35f2d303b273c2
Juan Quijano <jc_quijano@hotmail.com>
2025-02-25 13:05:04 +0100

    Test unitarios con Copilot Chat e inline

--- APL2007M4PrimeService/PrimeService.UnitTests/PrimeServiceTests.cs
+++ APL2007M4PrimeService/PrimeService.UnitTests/PrimeServiceTests.cs
@@ -1,8 +1,98 @@
 using System;
+using Xunit;
+using System.Numbers;
 
-namespace System.Numbers.UnitTests;
-
-public class PrimeServiceTests
+namespace System.Numbers.UnitTests
 {
+    public class PrimeServiceTests
+    {
+        private readonly PrimeService _primeService;
 
+        public PrimeServiceTests()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Fact]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.False(result, "1 should not be prime");
+        }
+
+        [Fact]
+        public void IsPrime_InputIs4_ReturnFalse()
+        {
+            var result = _primeService.IsPrime(4);
+
+            Assert.False(result, "4 should not be prime");
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(11)]
+        [InlineData(13)]
+        public void IsPrime_PrimeInput_ReturnTrue(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.True(result, $"{value} should be prime");
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(6)]
+        [InlineData(8)]
+        [InlineData(9)]
+        public void IsPrime_NonPrimeInput_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+
+        [Theory]
+        [InlineData(104729)] // Large prime number
+        [InlineData(1299709)] // Another large prime number
+        public void IsPrime_LargePrimeInput_ReturnTrue(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.True(result, $"{value} should be prime");
+        }
+
+        [Theory]
+        [InlineData(104728)] // Large non-prime number
+        [InlineData(1299708)] // Another large non-prime number
+        public void IsPrime_LargeNonPrimeInput_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(20)]
+        [InlineData(100)]
+        public void IsPrime_EvenNumbersGreaterThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+    }
 }
